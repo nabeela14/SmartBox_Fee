71ffb57c6717729ce35c74f3a4580b6f2d93c932
Nabeela Samar <59870832+nabeela14@users.noreply.github.com>
2021-05-29 20:57:45 +0530

    Update DBconnection.java

--- src/com/smartbox/DBconnection.java
+++ src/com/smartbox/DBconnection.java
@@ -7,7 +7,7 @@ public   class DBconnection {
         Connection conn =null;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Smartbox","root","rootnabee");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Smartbox","root","root");
         }catch (Exception e){
             e.printStackTrace();
         }
